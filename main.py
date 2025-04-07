bc0e111c925eba665d240769149bed3f67b1e37e
LeonardoBG2003 <148294983+LeonardoBG2003@users.noreply.github.com>
2025-04-07 08:22:11 -0600

    Clase anterior

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Importar la clase fastapi del framework#
 import uvicorn
 from fastapi import FastAPI
+from pymongo import MongoClient
+
+from dao.database import Conexion
 from routers import pedidosRouter, productosRouter, usuariosRouter
 
 #Crear una instancia de la clase fastapi#
@@ -13,6 +16,18 @@ app.include_router(usuariosRouter.router)
 async def home():
     salida = {"mensaje": "Bienvenido a PEDIDOSREST"}
     return salida
+
+@app.on_event("startup")
+async def startup():
+    print("Conectando con MongoDB")
+    conexion = Conexion()
+    app.conexion = conexion
+    app.db = conexion.getDB()
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Cerrando la conexion con MongoDB")
+    app.conexion.cerrar()
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', reload=True)
