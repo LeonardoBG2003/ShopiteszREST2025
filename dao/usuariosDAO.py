5f1af71c09d06a472f6eb78418082e9479ab5b66
LeonardoBG2003 <148294983+LeonardoBG2003@users.noreply.github.com>
2025-05-14 09:11:50 -0600

    Clase 14-05-2025
    Se realizo la autenticacion de el usuario por medio del correo y password, tomando en cuenta los 3 archivos principales.

--- dao/usuariosDAO.py
+++ dao/usuariosDAO.py
@@ -1,5 +1,5 @@
 from itertools import count
-
+from models.usuariosModel import UsuarioSalida
 
 class UsuarioDAO:
     def __init__(self, db):
@@ -22,3 +22,21 @@ class UsuarioDAO:
         except Exception as ex:
             print(ex)
         return count
+
+    def autenticar(self, email, password):
+        respuesta = UsuarioSalida(estatus="", mensaje="", usuario=None)
+        try:
+            usuario = self.db.usuarios.find_one({"email": email, "password": password, "estatus": "A"},
+                                                projection={"tarjetas": False})
+            if usuario:
+                respuesta.estatus = "OK"
+                respuesta.mensaje = "Usuario autenticado con exito"
+                respuesta.usuario = usuario
+            else:
+                respuesta.estatus = "ERROR"
+                respuesta.mensaje = "Datos incorrectos"
+        except Exception as ex:
+            print(ex)
+            respuesta.estatus = "ERROR"
+            respuesta.mensaje = "Error interno al autenticar el usuario, consulta al administrador"
+        return respuesta
--- routers/usuariosRouter.py
+++ routers/usuariosRouter.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from dao.usuariosDAO import UsuarioDAO
+from models.usuariosModel import Login, UsuarioSalida
 
 router = APIRouter(
     prefix="/usuarios",
     tags=["Usuarios"]
 )
 
-@router.get("/usuarios/login")
-async def login():
-    return {"mensaje": "Validando las credenciales del usuario"}
+@router.post("/autenticar", response_model = UsuarioSalida, summary="Autenticar un usuario")
+async def login(login: Login, request: Request) -> UsuarioSalida:
+    usuarioDAO = UsuarioDAO(request.app.db)
+    return usuarioDAO.autenticar(login.email, login.password)
